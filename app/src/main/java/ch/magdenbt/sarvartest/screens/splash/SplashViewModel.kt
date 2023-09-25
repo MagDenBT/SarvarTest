81c453271d5d2f6c247f675e645f9363470c03a9
Muhammad <magdenbt@gmail.com>
2023-09-25 16:56:25 +0300

    Fixed tasks cancellation by timeout

--- app/src/main/java/ch/magdenbt/sarvartest/screens/splash/SplashViewModel.kt
+++ app/src/main/java/ch/magdenbt/sarvartest/screens/splash/SplashViewModel.kt
@@ -4,69 +4,89 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import ch.magdenbt.sarvartest.common.RequestTimeOutException
 import ch.magdenbt.sarvartest.common.Resource
 import ch.magdenbt.sarvartest.datasource.DataSource
 import ch.magdenbt.sarvartest.model.Config
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withTimeoutOrNull
+import kotlinx.coroutines.withTimeout
 import timber.log.Timber
 
-class SplashViewModel(private val dataSource: DataSource, private val ioDispatcher: CoroutineDispatcher) : ViewModel() {
+class SplashViewModel(
+    private val dataSource: DataSource,
+    ioDispatcher: CoroutineDispatcher,
+) : ViewModel() {
 
-    private val _config: MutableStateFlow<Resource<Config>> = MutableStateFlow(Resource.Loading(null, 0))
+    private val _config: MutableStateFlow<Resource<Config>> =
+        MutableStateFlow(Resource.Loading(null, 0))
     val config = _config.asLiveData()
     private val _allTasksDone = MutableLiveData(false)
     val allTasksDone: LiveData<Boolean> = _allTasksDone
 
     init {
         CoroutineScope(ioDispatcher).launch {
-            val result = withTimeoutOrNull(5000L) {
-                Timber.d("Запуск получения конфига")
-                dataSource.getConfigWithProgress().collect {
-                    _config.value = it
-                }
+            withTimeout(SPLASH_SCREEN_TIMEOUT) {
+                startTimeoutSafetyConfigRequest(LOAD_CONFIG_TIMEOUT)
+                startTimeoutSafetyAsyncTasks(this)
             }
-            if (result == null) {
-                Timber.e("Завершено получение конфига с ошибкой из-за истечения отведенного")
-                _config.value = Resource.Error(RequestTimeOutException(), _config.value.data, _config.value.progress)
-            } else {
-                Timber.d("Завершено получение конфига без ошибок")
-            }
-
-            startAsyncTasks()
             _allTasksDone.postValue(true)
         }
     }
 
-    private fun startAsyncTasks() {
-        viewModelScope.launch(ioDispatcher) {
-            Timber.d("Запуск асинхронных задач")
-            val job1 = async {
-                launchAsyncTask(1)
-            }
-            val job2 = async {
-                launchAsyncTask(2)
-            }
-            val job3 = async {
-                launchAsyncTask(3)
-            }
+    private suspend fun startTimeoutSafetyConfigRequest(timeout: Long) {
+        try {
+            withTimeout(timeout) {
+                Timber.d("Запуск получения конфига")
 
-            awaitAll(job1, job2, job3)
-            Timber.d("Все асинхронные задачи завершены")
+                dataSource.getConfigWithProgress().collect {
+                    _config.value = it
+                }
+
+                Timber.w("Завершено получение конфига без ошибок")
+            }
+        } catch (ex: CancellationException) {
+            Timber.e("Завершено получение конфига с ошибкой из-за истечения отведенного")
+            _config.value = recreateResourceWithError(RequestTimeOutException())
         }
     }
 
+    private fun recreateResourceWithError(error: Throwable) = Resource.Error(
+        error,
+        _config.value.data,
+        _config.value.progress,
+    )
+
+    private suspend fun startTimeoutSafetyAsyncTasks(
+        coroutineScope: CoroutineScope,
+    ) {
+        try {
+            startAsyncTasks(coroutineScope)
+            Timber.w("Все асинхронные задачи завершены")
+        } catch (ex: CancellationException) {
+            coroutineScope.cancel()
+            Timber.e("Асинхронные не успели выполниться за отведенное время")
+        }
+    }
+
+    private suspend fun startAsyncTasks(coroutineScope: CoroutineScope) {
+        Timber.d("Запуск асинхронных задач")
+        val job1 = coroutineScope.async { launchAsyncTask(1) }
+        val job2 = coroutineScope.async { launchAsyncTask(2) }
+        val job3 = coroutineScope.async { launchAsyncTask(3) }
+        awaitAll(job1, job2, job3)
+    }
+
     private suspend fun launchAsyncTask(taskNumber: Int) {
-        val delayMillis = (100..10000).random()
+        val delayMillis = (ASYNC_TASK_TIMEOUT_RANGE_FROM..SPLASH_SCREEN_TIMEOUT).random()
+        delay(delayMillis)
         Timber.d("Асинхронная задача $taskNumber завершена")
-        delay(delayMillis.toLong())
     }
 }
---
+++ app/src/main/java/ch/magdenbt/sarvartest/screens/splash/TimeoutConstants.kt
@@ -0,0 +1,5 @@
+package ch.magdenbt.sarvartest.screens.splash
+
+const val SPLASH_SCREEN_TIMEOUT = 10000L
+const val LOAD_CONFIG_TIMEOUT = 5000L
+const val ASYNC_TASK_TIMEOUT_RANGE_FROM = 100L
